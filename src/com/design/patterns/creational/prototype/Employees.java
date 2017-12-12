206fffd910e0ecc912b7a778baced7e4c2026c05
Rhye Gacillos <rhye.gacillos@gmail.com>
2017-12-12 18:57:22 +0800

    Builder Pattern

---
+++ src/com/design/patterns/creational/prototype/Employees.java
@@ -0,0 +1,39 @@
+package com.design.patterns.creational.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees implements Cloneable {
+
+    private List<String> empList;
+
+    public Employees() {
+        empList = new ArrayList<>();
+    }
+
+    public Employees(List<String> list) {
+        this.empList = list;
+    }
+
+    public void loadData() {
+        // read all employees from database and put into the list
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("David");
+        empList.add("Lisa");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        List<String> temp = new ArrayList<>();
+        for (String s: this.getEmpList()) {
+            temp.add(s);
+        }
+
+        return new Employees(temp);
+    }
+}
