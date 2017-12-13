1590ab020c832b6c05c9570cb35371b9d746a246
Rhye Gacillos <rhye.gacillos@gmail.com>
2017-12-13 11:40:19 +0800

    Composite Pattern

---
+++ src/com/design/patterns/structural/composite/CompositePatternTestApp.java
@@ -0,0 +1,24 @@
+package com.design.patterns.structural.composite;
+
+public class CompositePatternTestApp {
+    public static void main(String[] args) {
+        Shape triangle1 = new Triangle();
+        Shape triangle2 = new Triangle();
+        Shape circle = new Circle();
+
+        Drawing drawing = new Drawing();
+        drawing.add(triangle1);
+        drawing.add(triangle2);
+        drawing.add(circle);
+
+        drawing.draw("Red");
+
+        drawing.clear();
+
+        drawing.add(triangle1);
+        drawing.add(triangle2);
+        drawing.add(circle);
+        drawing.draw("Green");
+
+    }
+}
---
+++ src/com/design/patterns/structural/composite/Triangle.java
@@ -0,0 +1,8 @@
+package com.design.patterns.structural.composite;
+
+public class Triangle implements Shape {
+    @Override
+    public void draw(String fillColor) {
+        System.out.println("Drawing Triangle with color " + fillColor);
+    }
+}
