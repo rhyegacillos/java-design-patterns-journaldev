998cb21185251703983321a00f1a63ae34d09c9c
Rhye Gacillos <rhye.gacillos@gmail.com>
2017-12-13 15:59:21 +0800

    Proxy Pattern - Additional example

---
+++ src/com/design/patterns/structural/proxy/another/sample/proxyClasses/ProxyPatternTestApp.java
@@ -0,0 +1,10 @@
+package com.design.patterns.structural.proxy.another.sample.proxyClasses;
+
+public class ProxyPatternTestApp {
+    public static void main(String[] args) {
+        System.out.println("****Proxy Pattern Demo****");
+
+        Proxy px = new Proxy();
+        px.doSomeWork();
+    }
+}
