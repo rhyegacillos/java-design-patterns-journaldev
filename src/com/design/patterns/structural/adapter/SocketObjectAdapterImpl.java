ed9dd6a8be0a545e56460fa14af595dd5962dd9e
Rhye Gacillos <rhye.gacillos@gmail.com>
2017-12-13 08:59:01 +0800

    Adapter Pattern

---
+++ src/com/design/patterns/structural/adapter/SocketObjectAdapterImpl.java
@@ -0,0 +1,28 @@
+package com.design.patterns.structural.adapter;
+
+// using composition for adapter pattern
+public class SocketObjectAdapterImpl implements SocketAdapter {
+
+    private Socket socket = new Socket();
+
+    @Override
+    public Volt get120Volt() {
+        return socket.getVolt();
+    }
+
+    @Override
+    public Volt get12Volt() {
+        Volt v = socket.getVolt();
+        return convertVolt(v, 10);
+    }
+
+    @Override
+    public Volt get3Volt() {
+        Volt v = socket.getVolt();
+        return convertVolt(v, 40);
+    }
+
+    private Volt convertVolt(Volt v, int i) {
+        return new Volt(v.getVolts() / i);
+    }
+}
